64a9614e17c1fe1ba78bd209b41706229473e929
HugoZap11 <hugozap11@gmail.com>
2026-04-28 17:38:36 +0200

    Update mockData.js

---
+++ server/mockData.js
@@ -0,0 +1,15 @@
+//Para pruebas de server WebSocket, se pueden usar estas credenciales para generar un JWT válido con la clave secreta definida en el .env, lo que permitirá acceder a las funcionalidades protegidas del servidor.
+
+const mockTank=[
+{
+    id:'game-001',
+    estado:'activa',
+    numronda:1,
+    jugadores:[
+        {
+            id:'player-001',
+            Tank:{"id":"tank-001","name":"HeavyTank","health":100,"position":{"x":0,"y":0},"color":"red"},
+}
+
+
+]
