865df092865b0f63a8dd92b35f2f0daec224e5cd
HugoZap11 <hugozap11@gmail.com>
2026-04-30 17:41:19 +0200

    Update gameHandler.js

--- server/Sockets/gameHandler.js
+++ server/Sockets/gameHandler.js
@@ -15,10 +15,71 @@ export default (io, socket) =>{
                 nombre: socket.user.nombre,
                 socket: socket.id,
             }],
-            mensajes:[`Bienvenido ${socket.user.nombre} a la partida ${gameId}`],
+            mensajes:[`Juego creado por ${socket.user.nombre} con id ${gameId}`],
             fechaInicio:new Date()
         };
+        socket.join(gameId); //El socket se une a la sala del juego
+        gameManager.save(gameId, newGame); //Guarda el juego en el gameManager
+        
+        io.to(gameId).emit('gameCreated', newGame); //Emite el evento a todos los sockets en la sala del juego
 
+        socket.emit('gameCreated', {id:gameId}); //Emite el evento al socket que creó el juego
+
+        io.emit('partidas', gameManager.getAll()); //Emite el evento a todos los sockets conectados con la lista de partidas actualizada    
+
+
+    };
+
+    const handleShoot=(payload)=>{
+        const{ gameId,targetTankId}=payload;
+        console.log(`Disparo a ${targetTankId} en el juego ${gameId}`);
+        const game=gameManager.get(gameId);
+        //Logica de validacion del disparo, por ejemplo, verificar si el tanque objetivo está en el juego y si el disparo es válido
+
+};
+
+const handleStartGame=(payload)=>{
+    const{ gameId, targetId}=payload;
+    const game = gameManager.get(gameId);
+
+    if(!game){
+        socket.emit('error', {message:'Juego no encontrado'});
+        return;
     }
 
-}
+    game.estado='en progreso';
+    gameManager.save(gameId, game); //Actualiza el estado del juego en el gameManager
+    socket.join(gameId); //El socket se une a la sala del juego
+    io.to(gameId).emit('gameStarted', game); //Emite el evento a todos los sockets en la sala del juego
+
+};
+
+const handleJoinGame=(payload)=>{
+    const{ gameId}=payload;
+    const game=gameManager.get(gameId);
+    if(!game){
+        socket.emit('error', {message:'Juego no encontrado'});
+        return;
+    }
+
+    //verifica si el usuario ya está en el juego
+    const jugadorEnJuego=game.jugadores.some(jugador=>jugador.id===socket.user.id);
+    if(jugadorEnJuego) return;
+
+    const nuevoJugador={
+        id:socket.user.id,
+        nombre: socket.user.nombre,
+        socket: socket.id,
+    };
+
+    game,jugadores.push(nuevoJugador);
+    gameManager.save(gameId, game);
+    socket.join(gameId); //El socket se une a la sala del juego
+    io.to(gameId).emit('playerJoined', game); //Emite el evento a todos los sockets en la sala del juego
+};
+
+    socket.on('createGame', handleCreateGame);
+    socket.on('shoot', handleShoot);
+    socket.on('startGame', handleStartGame);
+    socket.on('joinGame', handleJoinGame);
+};
